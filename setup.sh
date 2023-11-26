fe67edf4ee24d9be05c21ed5000dce38741f8841
davideciacciolo <davide.ciacciolo@outlook.com>
2023-11-26 19:21:04 +0100

    Error print format changed

--- setup.sh
+++ setup.sh
@@ -13,29 +13,55 @@ check_status() {
     fi
 }
 
+# TODO install rustup if not available
 # Check if rustup is available
 if ! command -v rustup &>/dev/null; then
-    echo "Error: rustup is not installed. Please install rustup before running this script."
+    echo "Error: rustup is not installed. Please install rustup before running this script"
     exit 1
 fi
 
-# Set Rust edition (stable)
-echo "Setting Rust edition to stable..."
-rustup default stable
-check_status $? "Failed to set Rust edition."
-echo "Rust edition has been set successfully."
+# TODO install cargo if not available
+# Check if cargo is available
+if ! command -v cargo &>/dev/null; then
+    echo "Error: Cargo is not installed. Please install Rust before running this script."
+    exit 1
+fi
 
-# TODO add printing and error handling for the next 2 lines
+# Set Rust edition interactively
+echo "Choose Rust edition:"
+select edition in "stable" "beta" "nightly"; do
+    case $edition in
+        "stable" | "beta" | "nightly")
+            echo "Setting Rust edition to $edition..."
+            rustup default $edition
+            check_status $? "Failed to set Rust edition"
+            break
+            ;;
+        *)
+            echo "Invalid selection, please choose a valid edition"
+            ;;
+    esac
+done
+
+# cargo-binutils
+echo "Installing 'cargo-binutils'..."
 cargo install cargo-binutils
+check_status $? "Failed to install 'cargo-binutils'"
+
+# llvm-tools-preview
+echo "Installing 'llvm-tools-preview'..."
 rustup component add llvm-tools-preview
+check_status $? "Failed to install 'llvm-tools-preview'"
+
 # TODO install the right version of gdb for embedded programming
 
 cortex_version="Cortex-M4F"
 target="thumbv7em-none-eabihf"
 echo "Adding target for ${cortex_version} ($target)..."
 rustup target add "$target"
-check_status $? "Failed to add the target for ${cortex_version} ($target)."
-echo "Rust target has been set up succesfully"
+check_status $? "Failed to add the target for ${cortex_version} ($target)"
 
 # TODO check if there is some tool missing for setup
+
+echo "Toolchain setup completed successfully"
 
